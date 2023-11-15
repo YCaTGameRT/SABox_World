2a3546b7e6fa0b9826e0376879054473594b5693
farfind <farfindnikolay@gmail.com>
2023-11-15 16:10:49 +0500

    added character movement

--- Scripts/BOXMan_control.gd
+++ Scripts/BOXMan_control.gd
@@ -1,11 +1,17 @@
-extends Node
+class_name BOXMan_control extends CharacterBody2D
 
+var speed = 14000
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	var move_up = Input.is_action_pressed("movement_up")
+	var move_down = Input.is_action_pressed("movement_down")
+	var move_right = Input.is_action_pressed("movement_right")
+	var move_left = Input.is_action_pressed("movement_left")
+	
+	var movement_vector = Vector2(0, 0)
+	
+	if move_up != move_down: movement_vector.y = 1 if move_down else -1
+	if move_right != move_left: movement_vector.x = 1 if move_right else -1
+	
+	velocity = movement_vector.normalized() * speed * delta
+	move_and_slide()
