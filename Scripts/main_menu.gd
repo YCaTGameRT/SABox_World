e10e5b77f5fdcbe48240ec3d885dcf303fd60772
farfind <farfindnikolay@gmail.com>
2023-11-18 14:02:35 +0500

    added fullscreen button

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -1,8 +1,20 @@
 extends Control
 
+@export var toggle_fullscreen_sprite: TextureRect
+@export var to_fullscreen_texture: Texture2D
+@export var to_window_texture: Texture2D
+
 func _process(delta):
 	if Input.is_action_just_pressed("exit"): quit()
 	if Input.is_action_just_pressed("play"): play()
+
+func toggle_fullscreen():
+	if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		toggle_fullscreen_sprite.texture = to_fullscreen_texture
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		toggle_fullscreen_sprite.texture = to_window_texture
 
 func play():
 	get_tree().change_scene_to_packed(load("res://scenes/sceneI.tscn"))
