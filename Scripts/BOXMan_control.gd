4140bbbf08eabb0ae3a8635fbcc04df2b88e7cb0
farfind <farfindnikolay@gmail.com>
2023-11-18 14:57:23 +0500

    added bridge (it works badly but works)

--- Scripts/BOXMan_control.gd
+++ Scripts/BOXMan_control.gd
@@ -19,3 +19,11 @@ func _process(delta):
 	
 	velocity = movement_vector.normalized() * speed
 	move_and_slide()
+
+func _on_bridge_detector_area_entered(area):
+	if area.get_meta("collision_tag") == "bridge":
+		set_collision_mask_value(2, false)
+
+func _on_bridge_detector_area_exited(area):
+	if area.get_meta("collision_tag") == "bridge":
+		set_collision_mask_value(2, true)
