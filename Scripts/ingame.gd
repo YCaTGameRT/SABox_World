100021a053ee2d3310b57cd19bcff2e09358b757
farfind <farfindnikolay@gmail.com>
2023-11-18 13:31:54 +0500

    added exit on escape

--- Scripts/ingame.gd
+++ Scripts/ingame.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+func _process(delta):
+	if Input.is_action_just_pressed("exit"): exit()
+
 func exit():
 	get_tree().change_scene_to_packed(load("res://scenes/main_menu.tscn"))
 
--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -1,7 +1,10 @@
 extends Control
 
+func _process(delta):
+	if Input.is_action_just_pressed("exit"): quit()
+
 func _on_play_pressed():
 	get_tree().change_scene_to_packed(load("res://scenes/sceneI.tscn"))
 
-func _on_quit_pressed():
+func quit():
 	get_tree().quit()
