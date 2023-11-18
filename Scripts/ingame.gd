9b25a918ec22e330c36ca2826cde66df037c4d12
farfind <farfindnikolay@gmail.com>
2023-11-18 13:29:34 +0500

    fixed scenes changing

--- Scripts/ingame.gd
+++ Scripts/ingame.gd
@@ -1,11 +1,7 @@
-extends Node
+extends Node2D
 
-@export var game_scene1: PackedScene
-
-var simultaneous_scene = preload("res://scenes/main_menu.tscn").instantiate()
-
-func _on_button_pressed():
-	get_tree().root.add_child(simultaneous_scene)
+func exit():
+	get_tree().change_scene_to_packed(load("res://scenes/main_menu.tscn"))
 
 
 
--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -1,9 +1,7 @@
 extends Control
 
-@export var game_scene: PackedScene
-
 func _on_play_pressed():
-	get_tree().change_scene_to_packed(game_scene)
+	get_tree().change_scene_to_packed(load("res://scenes/sceneI.tscn"))
 
 func _on_quit_pressed():
 	get_tree().quit()
