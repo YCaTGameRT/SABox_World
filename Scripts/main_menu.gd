0d2c661b357f6842df5c423ee09eed8aa84fd620
farfind <farfindnikolay@gmail.com>
2024-03-09 23:12:01 +0500

    hide fullscreen switch button on non-desktop platforms

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -1,14 +1,20 @@
 extends Control
 
+@export var toggle_fullscreen_button: Button
 @export var toggle_fullscreen_sprite: TextureRect
 @export var to_fullscreen_texture: Texture2D
 @export var to_window_texture: Texture2D
+
+func _ready():
+	if not OS.has_feature("pc"):
+		toggle_fullscreen_button.hide()
 
 func _process(delta):
 	if Input.is_action_just_pressed("exit"): quit()
 	if Input.is_action_just_pressed("play"): play()
 
 func toggle_fullscreen():
+	if not OS.has_feature("pc"): return
 	if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 		toggle_fullscreen_sprite.texture = to_fullscreen_texture
